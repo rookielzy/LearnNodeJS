3096f2b52188382ab4affc10cb3b69061792ad11
rookielzy <slimjetlzy@gmail.com>
2016-12-07 22:37:04 +0800

    基本功能设计

---
+++ myblog/routes/index.js
@@ -0,0 +1,9 @@
+module.exports = function(app) {
+    app.get('/', function(req, res) {
+        res.redirect('/posts');
+    });
+    app.use('signup', require('./signup'));
+    app.use('signin', require('./signin'));
+    app.use('posts', require('./posts'));
+    app.use('signout', require('./signout'));
+};
