bf4d33c3980ce114b42b5ada0d43f95c4e6c08aa
rookielzy <slimjetlzy@gmail.com>
2017-03-19 20:25:59 +0800

    Real Time Chat App

---
+++ RealTimeChatApp/README.md
@@ -0,0 +1,6 @@
+# Real Time Chat App
+
+## 主要使用了 Node.js Express Socket.io 的API功能进行实现
+
+## server.js 为服务器驱动文件
+## client.js 主要功能是监听，发射信息
---
+++ RealTimeChatApp/public/client.js
@@ -0,0 +1,20 @@
+// initializing socket, connection on server Connect to server using io.connect
+const socket = io.connect('http://localhost:7777');
+socket.on('connect', function(data) {
+    // If Connect succeed emit a message
+    socket.emit('join', 'Hello server from client');
+});
+
+// listener for 'thread' event, which updates messages
+
+socket.on('thread', function(data) {
+    $('#thread').append('<li>' + data + '</li>');
+});
+
+// sends message to server, resets & prevents default form action
+$('form').submit(function() {
+    var message = $('#message').val();
+    socket.emit('messages', message);
+    this.reset();
+    return false;
+});
