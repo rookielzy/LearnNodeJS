bc35f72c77ff41d4eea07054d6e58c996247c4a7
rookielzy <slimjetlzy@gmail.com>
2017-03-24 22:10:29 +0800

    todo List First Commit

---
+++ my-first-vue-project/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <h1 v-text="title"></h1>
+    <input v-model="newItem" v-on:keyup.enter="addNew">
+    <ul>
+      <li v-for="item in items" v-bind:class="{ Finised: item.isFinished}" v-on:click="checked(item)">
+        {{ item.label }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data: function() {
+    return {
+      title: 'This is a todo list',
+      items: [
+        {
+          label: 'coding',
+          isFinished: false
+        },
+        {
+          label: 'reading',
+          isFinished: true
+        }
+      ],
+      newItem: ''
+    }
+  },
+  methods: {
+    checked: function(item) {
+      item.isFinished = !item.isFinished
+    },
+    addNew: function(){
+      this.items.push({
+        label: this.newItem,
+        isFinished: false
+      })
+    }
+  }
+}
+</script>
+
+<style>
+html, body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+#app {
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+}
+.Finised {
+  text-decoration: underline;
+}
+</style>
