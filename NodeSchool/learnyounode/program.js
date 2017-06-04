0e10f2c65300313ba34dca22ae111bdc69b4f726
rookielzy <slimjetlzy@gmail.com>
2017-06-04 20:50:39 +0800

    My First Async IO

--- NodeSchool/learnyounode/program.js
+++ NodeSchool/learnyounode/program.js
@@ -1,4 +1,9 @@
 const fs = require("fs");
+const file = process.argv[2];
 
-var contentArray = (fs.readFileSync(process.argv[2])).toString().split('\n');
-console.log(contentArray.length - 1);
+fs.readFile(file, (err, data) => {
+  if (err) throw err;
+
+  let contents = data.toString().split('\n');
+  console.log(contents.length - 1);
+});
