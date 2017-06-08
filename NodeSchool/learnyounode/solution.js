0a275806b5a7b8f935e64341cd4873ed038f5c54
rookielzy <slimjetlzy@gmail.com>
2017-06-08 22:49:08 +0800

    File Filter Modular

---
+++ NodeSchool/learnyounode/solution.js
@@ -0,0 +1,13 @@
+const filterFn = require('./solution_filter');
+const dir = process.argv[2];
+const filterStr = process.argv[3];
+
+filterFn(dir, filterStr, (err, list) => {
+    if (err) {
+        return console.error('There was an error: ', err);
+    }
+
+    list.forEach(file => {
+        console.log(file);
+    })
+})
