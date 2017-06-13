a3437c1ce8cab2eac8ec5c76223adc78104c411a
rookielzy <slimjetlzy@gmail.com>
2017-06-13 23:37:46 +0800

    About Jade

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // const rookielzy = {name: 'Jet', age: 23, cool: true };
+  // const dog = "O";
+  // res.send('Hey! It works!');
+  // res.json(rookielzy);
+  res.render('hello');
 });
 
 module.exports = router;
